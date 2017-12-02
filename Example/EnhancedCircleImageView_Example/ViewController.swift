8984304510cfcdad3e1398d6af42741d1d121450
saku <saku2saku@gmail.com>
2017-12-02 20:50:52 +0900

    Add sample code.

--- Example/EnhancedCircleImageView_Example/ViewController.swift
+++ Example/EnhancedCircleImageView_Example/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 import EnhancedCircleImageView
 
 class ViewController: UIViewController {
-    @IBOutlet private weak var imageView: EnhancedCircleImageView!
+    @IBOutlet private weak var heightImageView: EnhancedCircleImageView!
+    @IBOutlet private weak var borderImageView: EnhancedCircleImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.heightImageView.image = UIImage(named: "rectancle_height")
+        self.borderImageView.image = UIImage(named: "rectancle_width")
+        self.borderImageView.layer.borderWidth = 3.0
+        self.borderImageView.layer.borderColor = UIColor.red.cgColor
     }
 }
 
